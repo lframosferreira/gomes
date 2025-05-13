b1b5b7c7c62273b81b8a9435629ffe5107156c40
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2025-05-13 10:13:53 -0300

    adding some kind of state to flappy

--- flappy/main.go
+++ flappy/main.go
@@ -45,6 +45,7 @@ type Game struct {
 	Pipes          []Pipe
 	Score          int
 	LastAddedPipe  time.Time
+	Started        bool
 }
 
 func (g *Game) OutOfBounds() bool {
@@ -59,15 +60,25 @@ func (g *Game) Update() error {
 	switch {
 	case ebiten.IsKeyPressed(ebiten.KeySpace):
 		g.BirdSpeed = INITIAL_BIRD_JUMP_SPEED
+		g.Started = true
 	default:
 	}
 	g.BirdCoordinate.Y -= g.BirdSpeed
-	g.BirdSpeed -= GRAVITY
-	if g.OutOfBounds() {
-		return errors.New("Bird out of bounds!")
+	if g.Started {
+		g.BirdSpeed -= GRAVITY
 	}
-	for i, _ := range g.Pipes {
-		g.Pipes[i].X -= PIPE_SPEED
+	if g.OutOfBounds() {
+		return errors.New("Bird out of bounds")
+	}
+	if g.Started {
+		upper_pipe_size := rand.IntN(WINDOW_HEIGHT / 2)
+		if time.Since(g.LastAddedPipe) > time.Second*2 {
+			g.Pipes = append(g.Pipes, Pipe{Coordinate{X: WINDOW_WIDTH, Y: 0}, upper_pipe_size})
+			g.LastAddedPipe = time.Now()
+		}
+		for i := range g.Pipes {
+			g.Pipes[i].X -= PIPE_SPEED
+		}
 	}
 	return nil
 }
@@ -82,11 +93,6 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		x, y := pipe.X, pipe.Y
 		vector.DrawFilledRect(screen, float32(x), float32(y), float32(PIPE_WIDTH), float32(pipe.Size), GREEN, false)
 		vector.DrawFilledRect(screen, float32(x), float32(y+pipe.Size+PIPE_EMPTY_SIZE), float32(PIPE_WIDTH), float32(WINDOW_HEIGHT-pipe.Size-PIPE_EMPTY_SIZE), GREEN, false)
-	}
-	upper_pipe_size := rand.IntN(WINDOW_HEIGHT / 2)
-	if time.Since(g.LastAddedPipe) > time.Second*2 {
-		g.Pipes = append(g.Pipes, Pipe{Coordinate{X: WINDOW_WIDTH, Y: 0}, upper_pipe_size})
-		g.LastAddedPipe = time.Now()
 	}
 
 }
